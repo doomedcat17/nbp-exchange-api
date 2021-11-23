b1aae5d1d3a99fc76a3d71f46525d933acdc9c43
doomedcat17 <doomedcat17@outlook.com>
2021-11-23 19:09:55 +0100

    NbpCurrencyProvider interface added

--- src/main/java/com/doomedcat17/nbpexchangeapi/data/nbp/NbpCurrency.java
+++ src/main/java/com/doomedcat17/nbpexchangeapi/data/nbp/NbpCurrency.java
@@ -15,4 +15,22 @@ public class NbpCurrency {
     private BigDecimal midRateInPLN;
 
     private LocalDate effectiveDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NbpCurrency that = (NbpCurrency) o;
+
+        if (!name.equals(that.name)) return false;
+        return effectiveDate.equals(that.effectiveDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + effectiveDate.hashCode();
+        return result;
+    }
 }
---
+++ src/main/java/com/doomedcat17/nbpexchangeapi/data/nbp/provider/NpbCurrencyProvider.java
@@ -0,0 +1,4 @@
+package com.doomedcat17.nbpexchangeapi.data.nbp.provider;
+
+public interface NpbCurrencyProvider {
+}
