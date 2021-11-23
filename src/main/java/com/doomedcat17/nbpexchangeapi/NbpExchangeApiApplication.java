ce9f5d2e5d3a19a300636e78d88cf8cdf58636a2
doomedcat17 <doomedcat17@outlook.com>
2021-11-23 17:03:42 +0100

    init commit

---
+++ src/main/java/com/doomedcat17/nbpexchangeapi/NbpExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.doomedcat17.nbpexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbpExchangeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doomedcat17/nbpexchangeapi/NbpExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doomedcat17.nbpexchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpExchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
