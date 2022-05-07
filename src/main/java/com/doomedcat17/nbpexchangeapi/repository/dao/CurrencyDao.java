654abe0d8eb22ebe2c4c2b5f7acc1179fdd26824
Piotr Bojczewski <piotr200310@outlook.com>
2022-05-07 10:45:15 +0200

    add repository annotation

--- src/main/java/com/doomedcat17/nbpexchangeapi/repository/dao/CurrencyDao.java
+++ src/main/java/com/doomedcat17/nbpexchangeapi/repository/dao/CurrencyDao.java
@@ -2,6 +2,8 @@ package com.doomedcat17.nbpexchangeapi.repository.dao;
 
 import com.doomedcat17.nbpexchangeapi.data.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrencyDao extends JpaRepository<Currency, String> {
 }
