b0a484ef3351c81e1b00b59a1492619b7fa48515
Piotr Bojczewski <piotr200310@outlook.com>
2022-07-12 07:20:56 +0200

    add mapstruct NbpExchangeRateMapper

--- src/main/java/com/doomedcat17/nbpexchangeapi/mapper/NbpExchangeRateMapper.java
+++ src/main/java/com/doomedcat17/nbpexchangeapi/mapper/NbpExchangeRateMapper.java
@@ -1,10 +1,12 @@
 package com.doomedcat17.nbpexchangeapi.mapper;
 
 import com.doomedcat17.nbpexchangeapi.data.NbpExchangeRate;
+import com.doomedcat17.nbpexchangeapi.data.dto.RateDTO;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.mapstruct.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN, injectionStrategy = InjectionStrategy.CONSTRUCTOR, uses = {CurrencyMapper.class})
@@ -14,6 +16,15 @@ public interface NbpExchangeRateMapper {
     @Mapping(target = "effectiveDate", source = "effectiveDate")
     @Mapping(target = "currency", source = "jsonNode")
     NbpExchangeRate fromJson(JsonNode jsonNode, LocalDate effectiveDate);
+
+    @Mapping(target = "rate", source = "exchangeRateToMap", qualifiedByName = "rate")
+    RateDTO toRateDto(NbpExchangeRate exchangeRateToMap, @Context NbpExchangeRate baseExchangeRate);
+
+    @Named("rate")
+    default BigDecimal mapRate(NbpExchangeRate exchangeRateToMap, @Context NbpExchangeRate baseExchangeRate) {
+        return baseExchangeRate.getMidRateInPLN()
+                .divide(exchangeRateToMap.getMidRateInPLN(), RoundingMode.HALF_EVEN);
+    }
 
     @Named("midRateInPLN")
     default BigDecimal mapMid(JsonNode jsonNode) {
