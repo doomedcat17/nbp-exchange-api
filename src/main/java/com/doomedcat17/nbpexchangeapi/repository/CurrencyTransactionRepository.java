98ec081f3e9364d114a93085c620ffdf8872a76c
Piotr Bojczewski <piotr200310@outlook.com>
2022-07-14 19:11:09 +0200

    remove CurrencyTransactionRepository useless methods

--- src/main/java/com/doomedcat17/nbpexchangeapi/repository/CurrencyTransactionRepository.java
+++ src/main/java/com/doomedcat17/nbpexchangeapi/repository/CurrencyTransactionRepository.java
@@ -7,17 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface CurrencyTransactionRepository extends JpaRepository<CurrencyTransaction, Long> {
-
-    CurrencyTransaction getTopByOrderByDateDesc();
-
-    @Query("SELECT transaction FROM CurrencyTransaction transaction WHERE YEAR(transaction.date) = :year AND MONTH(transaction.date) = :month AND DAY(transaction.date) = :day")
-    List<CurrencyTransaction> getAllByDateYearAndDateMonthAndDateDay(@Param("year") Integer year, @Param("month") Integer month, @Param("day") Integer day, Pageable pageable);
 
     @Query("SELECT transaction FROM CurrencyTransaction transaction WHERE transaction.date BETWEEN :startDate AND :endDate")
     Page<CurrencyTransaction> getAllBetweenDates(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
